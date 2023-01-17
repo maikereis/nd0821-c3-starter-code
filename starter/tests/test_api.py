823a9051a38aaae21beb6bd58632b7b71d60bdcd
maike <maikerdralcantara@gmail.com>
2023-01-16 21:05:09 -0300

    more tests

--- starter/tests/test_api.py
+++ starter/tests/test_api.py
@@ -28,6 +28,27 @@ def data():
     return data
 
 
+@pytest.fixture
+def invalid_data():
+    invalid_data = {
+        "age": "not_a_number",
+        "workclass": "",
+        "fnlgt": "not_a_number",
+        "education": "",
+        "education_num": "not_a_number",
+        "marital_status": "",
+        "occupation": "",
+        "relationship": "",
+        "race": "",
+        "sex": "",
+        "capital_gain": "not_a_number",
+        "capital_loss": "not_a_number",
+        "hours_per_week": "not_a_number",
+        "native_country": "",
+    }
+    return invalid_data
+
+
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
@@ -48,3 +69,11 @@ def test_inference(data):
 def test_inference_content_type(data):
     response = client.post("/inference", json=data)
     assert response.headers["Content-Type"] == "application/json"
+
+
+def test_inference_invalid_data(invalid_data):
+    response = client.post("/inference", json=invalid_data)
+    assert response.status_code == 422
+    assert (
+        json.loads(response.text)["detail"][0]["msg"] == "value is not a valid integer"
+    )
