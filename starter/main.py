31ec4d11cc30e7502b1d064ef336737d56cac9d3
maike <maikerdralcantara@gmail.com>
2023-01-17 20:41:28 -0300

    add actions workflow

--- starter/main.py
+++ starter/main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 import numpy as np
 import pandas as pd
@@ -9,6 +10,13 @@ from sklearn.compose import make_column_transformer
 from sklearn.pipeline import make_pipeline
 
 app = FastAPI()
+
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 CURRENT_DIR = Path(__file__).parent
 ARTIFACTS_DIR = CURRENT_DIR / "model"
